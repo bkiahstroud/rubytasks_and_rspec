ffd62dc42d8eb83cf2f1a56f13bf3aeead159958
learn <learn@learn11.local>
2017-10-27 16:00:17 -0700

    added due dates and sorting

--- rubytasks.rb
+++ rubytasks.rb
@@ -1,13 +1,14 @@
 require 'date'
 class TaskList
-    attr_accessor :tasklist, :complete_list, :incomplete_list
+    attr_accessor :tasklist, :complete_list, :incomplete_list, :due_tasks
     def initialize
         @tasklist = []
         @complete_list = []
         @incomplete_list = []
+        @due_tasks = []
     end
     def add_task(t)
-        if t.class != Task
+        if t.class != DueDateTask
             raise RuntimeError.new("Must be a task")
         else
             @tasklist << t
@@ -17,17 +18,23 @@ class TaskList
         @tasklist
     end
     def show_complete_tasks(t, v)
-        if t.class != Task
+        if t.class != DueDateTask
             raise RuntimeError.new("Must be a complete task")
         elsif v == true
             @complete_list << t
         end
     end
     def show_incomplete_tasks(t, v)
-        if t.class != Task
+        if t.class != DueDateTask
             raise RuntimeError.new("Must be an incomplete task")
         elsif v == false
             @incomplete_list << t
+        end
+        @incomplete_list = @incomplete_list.sort_by{|object| object.due_date}
+    end
+    def show_due_tasks(t)
+        if t.show_due_date == Date.today
+            @due_tasks << t
         end
     end
 end
@@ -65,45 +72,61 @@ class DueDateTask < Task
     attr_accessor :due_date
     def initialize(due_date=Date.today+3)
         @due_date = due_date
+        super()
     end
-    def super
-        super
+    def show_due_date
+        @due_date
+    end
+    def change_due_date(due_date)
+        @due_date = due_date
+    end
+    def remove_date_from_completed_task
+        if @check_status == true
+            remove_instance_variable(:@due_date)
+        end
     end
 end
 
 tasklist = TaskList.new
-
-task1 = Task.new
+task1 = DueDateTask.new
 task1.change_title("Title1")
 task1.change_description("Descripton1")
-task1.toggle_mark
+task1.change_due_date(Date.today+1)
+task1.remove_date_from_completed_task
+tasklist.show_due_tasks(task1)
 
-task2 = Task.new
+task2 = DueDateTask.new
 task2.change_title("Title2")
 task2.change_description("Descripton2")
-# task2.toggle_mark
+task2.remove_date_from_completed_task
+tasklist.show_due_tasks(task2)
 
-task3 = Task.new
+task3 = DueDateTask.new
 task3.change_title("Title3")
 task3.change_description("Descripton3")
+task3.change_due_date(Date.today)
+task3.remove_date_from_completed_task
+tasklist.show_due_tasks(task3)
 
 task4 = DueDateTask.new
 task4.change_title("Title4")
 task4.change_description("Description4")
-p task4
+task4.change_due_date(Date.today-10)
+task4.remove_date_from_completed_task
+tasklist.show_due_tasks(task4)
 
-# tasklist.add_task(task1)
-# tasklist.add_task(task2)
-# tasklist.add_task(task3)
+p tasklist.due_tasks
+
 tasklist.show_complete_tasks(task1, task1.show_check_status)
 tasklist.show_complete_tasks(task2, task2.show_check_status)
 tasklist.show_complete_tasks(task3, task3.show_check_status)
+tasklist.show_complete_tasks(task4, task4.show_check_status)
 
 tasklist.show_incomplete_tasks(task1, task1.show_check_status)
 tasklist.show_incomplete_tasks(task2, task2.show_check_status)
 tasklist.show_incomplete_tasks(task3, task3.show_check_status)
+tasklist.show_incomplete_tasks(task4, task4.show_check_status)
 
-# p tasklist.complete_list
-# p tasklist.incomplete_list
-# tasklist.show_tasks
-p Date.today
+p tasklist.complete_list
+p " "
+p tasklist.incomplete_list
